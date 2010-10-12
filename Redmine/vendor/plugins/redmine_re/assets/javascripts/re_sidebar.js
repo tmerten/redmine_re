ff17b836c7ddba21f6447b3fa9480c5a7c5ace7e
tmerte2m <thorsten.merten@h-brs.de>
2010-10-12 08:10:37 +0000

    updated html and css to pass the w3c validator (http://validator.w3.org).

--- Redmine/vendor/plugins/redmine_re/assets/javascripts/re_sidebar.js
+++ Redmine/vendor/plugins/redmine_re/assets/javascripts/re_sidebar.js
@@ -1,8 +1,11 @@
-var ReSideBar;
 
 /*
-This
+This class initializes and slides the bar to the right.
+
+it uses the css "stylesheets/redmine_re.css" TODO: create my own file
+and the images "images/sidebar/*.gif"
  */
+var ReSideBar;
 ReSideBar = Class.create({
 
     isExtended : false,
@@ -13,17 +16,16 @@ ReSideBar = Class.create({
     },
 
     slide: function() {
-        new Effect.toggle('reSideBarContents', 'blind', {duration: 0.0, scaleX: 'true', scaleY: 'true;', scaleContent: false});
+        new Effect.toggle('reSideBarContents', 'blind', {duration: 0.25, scaleX: 'true', scaleY: 'true;', scaleContent: false});
         if (this.isExtended == 0) {
             $('reSideBarTab').childNodes[0].src = $('reSideBarTab').childNodes[0].src.replace(/(\.[^.]+)$/, '-active$1');
-            new Effect.Fade('reSideBarContents',
-            { duration:0.25, from:0.0, to:1.0 });
-            this.isExtended = 1;
+            /*$('reSideBarTab').CHANGEWIDTH TODO */
+            new Effect.Fade('reSideBarContents', { duration:0.25, from:0.0, to:1.0 });
+            this.isExtended ++;
         }
         else {
             $('reSideBarTab').childNodes[0].src = $('reSideBarTab').childNodes[0].src.replace(/-active(\.[^.]+)$/, '$1');
-            new Effect.Fade('reSideBarContents',
-            { duration:0.25, from:1.0, to:0.0 });
+            new Effect.Fade('reSideBarContents', { duration:0.25, from:1.0, to:0.0 });
             this.isExtended = 0;
         }
     }
