abeaa1dda8e0b1ab656d00842f1d543bf8a8ac77
tmerte2m <thorsten.merten@h-brs.de>
2010-10-12 09:34:05 +0000

    sidebar now working. moved css and js in extra files.

--- Redmine/vendor/plugins/redmine_re/assets/javascripts/re_sidebar.js
+++ Redmine/vendor/plugins/redmine_re/assets/javascripts/re_sidebar.js
@@ -1,32 +1,29 @@
-
 /*
-This class initializes and slides the bar to the right.
+ This class initializes and slides the bar to the right.
 
-it uses the css "stylesheets/redmine_re.css" TODO: create my own file
-and the images "images/sidebar/*.gif"
+ it uses the css "stylesheets/redmine_re.css" TODO: create my own file
+ and the images "images/sidebar/*.gif"
  */
 var ReSideBar;
 ReSideBar = Class.create({
 
-    isExtended : false,
-    
     initialize: function(id) {
         Event.observe(id, 'click', this.slide, true);
-        this.isExtended = 0;
     },
 
     slide: function() {
-        new Effect.toggle('reSideBarContents', 'blind', {duration: 0.25, scaleX: 'true', scaleY: 'true;', scaleContent: false});
-        if (this.isExtended == 0) {
-            $('reSideBarTab').childNodes[0].src = $('reSideBarTab').childNodes[0].src.replace(/(\.[^.]+)$/, '-active$1');
-            /*$('reSideBarTab').CHANGEWIDTH TODO */
-            new Effect.Fade('reSideBarContents', { duration:0.25, from:0.0, to:1.0 });
-            this.isExtended ++;
+        var image_name;
+        image_name = $$('#reSideBarTab img')[0].src;
+        if ( this.extended === undefined || this.extended === 0) {
+            $$('#reSideBarTab img')[0].src = image_name.replace(/(\.[^.]+)$/, '-active$1');
+            this.extended = 1;
         }
         else {
-            $('reSideBarTab').childNodes[0].src = $('reSideBarTab').childNodes[0].src.replace(/-active(\.[^.]+)$/, '$1');
-            new Effect.Fade('reSideBarContents', { duration:0.25, from:1.0, to:0.0 });
-            this.isExtended = 0;
+            $$('#reSideBarTab img')[0].src = image_name.replace(/-active(\.[^.]+)$/, '$1');
+            this.extended = 0;
         }
+
+        new Effect.toggle('reSideBarContents', 'blind', {duration: 0.25, scaleX: 'true', scaleY: 'true;', scaleContent: false});
+
     }
 });
