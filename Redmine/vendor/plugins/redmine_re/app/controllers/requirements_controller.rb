4fd181bbdefba56e03d79a6dfed3282d65b13ea8
tmerte2m <thorsten.merten@h-brs.de>
2010-11-23 09:58:06 +0000

    Tree switch completed. The new tree is simpler and more powerful. Contextmenu and links are still missing.

--- Redmine/vendor/plugins/redmine_re/app/controllers/requirements_controller.rb
+++ Redmine/vendor/plugins/redmine_re/app/controllers/requirements_controller.rb
@@ -8,41 +8,25 @@ class RequirementsController < RedmineReController
   def index
     @artifacts  = ReArtifactProperties.find_all_by_project_id(@project.id)
     @artifacts = [] if @artifacts == nil
-    # jsontree will start with project as one and only node
-    @jsontree = '[ {"id": "project_' + @project.id.to_s + '", "txt" : "' + @project.name.to_s + '",'
-    @jsontree += '"onclick":false, "ondblclick":false,'
-    @jsontree += '"items" : ['
 
     @htmltree = '<ul id="tree">'
-    #if params[:id]
-      # Create only one branch starting with artifact with given id if id is given
-     # artifacts << ReArtifactProperties.find_by_id_and_project_id(params[:id], @project.id)
-    #else  #TODO : wenn realtionship table funktioniert wieder zu laufen bringen:
-     # artifacts += ReArtifactProperties.find_all_by_parent_artifact_id_and_project_id(nil, @project.id)
-    #end
     for artifact in @artifacts
-      
       if (artifact.parent.nil?)
-        render_to_json_tree(artifact)
         render_to_html_tree(artifact)
-        if (artifact != @artifacts.last)
-          @jsontree += ","
-        end
       end
     end
-    @jsontree += "] } ]"
     @htmltree += '</ul>'
   end
 
   ##
-  # The following method is called via JavaScript Tafeltree by an ajax request.
+  # The following method is called via JavaScript Tree by an ajax request.
   # It transmits the drops done in the tree to the database in order to last
   # longer than the next refresh of the browser.
   def delegate_tree_drop
     new_parent_id = params[:new_parent_id]
     moved_artifact_id = params[:moved_artifact_id]
     child = ReArtifactProperties.find_by_id(moved_artifact_id)
-    if new_parent_id.index('project') != nil
+    if new_parent_id == 'null'
       # Element is dropped under root node which is the project new parent-id has to become nil.
       child.parent = nil
     else
@@ -55,54 +39,14 @@ class RequirementsController < RedmineReController
   end
 
   ##
-  # The following method is called via JavaScript Tafeltree by an ajax update request.
+  # The following method is called via JavaScript Tree by an ajax update request.
   # It transmits the call to the according controller which should render the detail view
   def delegate_tree_node_click
     artifact = ReArtifactProperties.find_by_id(params[:id])
     redirect_to url_for :controller => params[:artifact_controller], :action => 'edit', :id => params[:id], :parent_id => artifact.parent_artifact_id, :project_id => artifact.project_id
   end
 
-  ##
-  # this methods renders any re_artifact_properties
-  # as a tree
-  # --
-  # TODO: re_artifact_properties has no child(ren)! think of a sound solution. Hints:Next line
-  # * one could use a document structure (see Unicase) to sort artifacts in a tree
-  # * one could use a fixed (maybe even variable) structure to display tree-like stuff
-  #  * e.g. Workarea -> Task -> Subtask
-  # * one could use (I should refactor "one could use a") a pre-definable parent-child-model for automatically sorting
-  #   artifacts
-  # --> first solution: Each re_artifact_properties can have a child of any type. This is implemented as a basic version.
-  # Later on one can restrict which artifact can have which kind of children (maybe even by the help of a
-  # configuration file.
-  # ++
-  def render_to_json_tree(re_artifact)
-    @jsontree += '{'
-    @jsontree += '"id" : "' + re_artifact.id.to_s + '"'
-    @jsontree += ', "txt" : "' + re_artifact.name.to_s    # for tests: + re_artifact_properties.id.to_s + ' parent: ' + re_artifact_properties.parent_artifact_id.to_s
-    #@jsontree += '<a href="http://gmx.de">x</a>' # this won't work! only onclick event works!
-    @jsontree += '"'
-    #@jsontree += ', "ondrop" : "tree_node_drop"'
-
-    #@jsontree += ', "onclick" : "tree_node_click(' + re_artifact_properties.id.to_s + ')"'
-    # it did not work, when specifying a full function call with arguments!
-
-    # like this it works
-    @jsontree += ', "img" : "' + re_artifact.artifact_type.to_s.underscore.concat('.gif" ')
-    if (!re_artifact.children.empty?)
-      @jsontree += ', "items" : ['
-      for child in re_artifact.children
-        render_to_json_tree(child)
-        if (child != re_artifact.children.last)
-          @jsontree += ','
-        end
-      end
-      @jsontree += ']'
-    end
-    @jsontree += '}'
-  end
-
-
+  #renders a re artifact and its children recursively as html tree
   def render_to_html_tree(re_artifact)
     @htmltree += '<li id="node_' + re_artifact.id.to_s #IDs must begin with a letter(!)
     @htmltree += '" class="' + re_artifact.artifact_type.to_s.underscore + '">'
@@ -130,6 +74,5 @@ class RequirementsController < RedmineReController
 
     render :layout => false
   end
-
 
 end
