1f172e1366ad1592907332d94f009f16f1312ccc
djuepp2s <daniela.jueppner@smail.inf.h-brs.de>
2010-12-02 12:42:05 +0000

    Bei Auswahl von "Select Artifact" aus der Auswahlliste passiert einfach nix mehr, anstatt dass ein Fehler gerendert wird.

--- Redmine/vendor/plugins/redmine_re/app/controllers/re_artifact_relationship_controller.rb
+++ Redmine/vendor/plugins/redmine_re/app/controllers/re_artifact_relationship_controller.rb
@@ -3,9 +3,11 @@ class ReArtifactRelationshipController < ApplicationController
 
   def prepare_relationships
     @artifact_properties_id = ReArtifactProperties.get_properties_id(params[:original_controller], params[:id])
-    @source = ReArtifactProperties.find_by_id(@artifact_properties_id)
-    @sink = ReArtifactProperties.find_by_id(params[:sink_id])
-    @source.relate_to(@sink, :conflict, false)
+    if params[:sink_id] != ""
+      @source = ReArtifactProperties.find_by_id(@artifact_properties_id)
+      @sink = ReArtifactProperties.find_by_id(params[:sink_id])
+      @source.relate_to(@sink, :conflict, false)
+    end
     @relationships_outgoing = ReArtifactRelationship.find_all_by_source_id(@artifact_properties_id)
     @relationships_incoming = ReArtifactRelationship.find_all_by_sink_id(@artifact_properties_id)
     render :partial => "relationship_links", :layout => false, :project_id => params[:project_id]
