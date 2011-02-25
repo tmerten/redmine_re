c56fffe66ad8d04485aa4d048b773b367a545adf
tmerte2m <thorsten.merten@h-brs.de>
2011-02-25 10:48:25 +0000

    closes #47 This is the final fix for bug 47

--- Redmine/vendor/plugins/redmine_re/app/helpers/application_helper.rb
+++ Redmine/vendor/plugins/redmine_re/app/helpers/application_helper.rb
@@ -8,4 +8,36 @@ module ApplicationHelper
     end
     s
   end
+
+	def number_field_with_slider(objectname, method, min, max)
+		fieldid = objectname.to_s + "_" + method.to_s
+
+		js = <<JAVASCRIPT 
+		Event.observe(window, 'load', function() {
+      var #{fieldid}Slider = new Control.Slider('#{fieldid}-handle' , '#{fieldid}-track',
+      {
+				range: $R(1,51),
+				values: $R(1,50),
+				sliderValue: $('#{fieldid}').value,
+				onChange: function(v) { $('#{fieldid}').value = v; },
+				onSlide:  function(v) { $('#{fieldid}').value = v; }
+      } );
+      
+			$('#{fieldid}').observe('change', function() {
+				if (this.value < #{min}) this.value = #{min};
+				if (this.value > #{max}) this.value = #{max};
+				#{fieldid}Slider.setValue(this.value);  
+			});
+	  });
+JAVASCRIPT
+  
+		js = javascript_tag(js)
+
+		sliderdivs = content_tag("div", "", :id => "#{fieldid}-handle", :class => "numberfield-handle")
+    sliderdivs = content_tag("div", sliderdivs, :id => "#{fieldid}-track", :class => "numberfield-track")
+	  field = label(objectname, method, t(fieldid))		
+		field << text_field(objectname, method, :size => 3)
+    sliderdivs = content_tag(:div, field+sliderdivs, :class => "numberfield-slider")
+    js + sliderdivs
+	end
 end
