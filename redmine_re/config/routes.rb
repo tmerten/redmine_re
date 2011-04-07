5546b86704f1db4c582dcc711404920cf518a296
tmerte2m <thorsten.merten@h-brs.de>
2011-04-07 12:49:47 +0000

    slight cosmetics

--- redmine_re/config/routes.rb
+++ redmine_re/config/routes.rb
@@ -1,12 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :requirements, :path_prefix => '/projects/:project_id'
-  map.resources :re_goal, :path_prefix => '/projects/:project_id', :only => :new
-  map.resources :re_section, :path_prefix => '/projects/:project_id', :only => :new
-  map.resources :re_subtask, :path_prefix => '/projects/:project_id', :only => :new
-  map.resources :re_task , :path_prefix => '/projects/:project_id', :only => :new
-  map.resources :re_user_profile, :path_prefix => '/projects/:project_id', :only => :new
-  map.resources :re_attachment, :path_prefix => '/projects/:project_id', :only => :new
-  map.resources :re_vision, :path_prefix => '/projects/:project_id', :only => :new
-  map.resources :re_workarea, :path_prefix => '/projects/:project_id', :only => :new
-  map.resources :re_artifact_relationship, :path_prefix => '/projects/:project_id', :only => :visualization
+  map.resources :re_goal, :path_prefix => '/projects/:project_id'
+  map.resources :re_section, :path_prefix => '/projects/:project_id'
+  map.resources :re_subtask, :path_prefix => '/projects/:project_id'
+  map.resources :re_task , :path_prefix => '/projects/:project_id'
+  map.resources :re_user_profile, :path_prefix => '/projects/:project_id'
+  map.resources :re_attachment, :path_prefix => '/projects/:project_id'
+  map.resources :re_vision, :path_prefix => '/projects/:project_id'
+  map.resources :re_workarea, :path_prefix => '/projects/:project_id'
+  map.resources :re_artifact_relationship, :path_prefix => '/projects/:project_id'
 end
