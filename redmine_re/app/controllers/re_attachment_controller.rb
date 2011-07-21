380f736efef534cce67d1f2cea464c587a8f30c5
dmarko2s <dominic.markowski@smail.inf.h-brs.de>
2011-07-21 11:44:36 +0000

    Updated and works with more then 1 file

--- redmine_re/app/controllers/re_attachment_controller.rb
+++ redmine_re/app/controllers/re_attachment_controller.rb
@@ -1,33 +1,16 @@
 class ReAttachmentController < RedmineReController
   unloadable
 
-  def edit
-    @re_attachment = ReAttachment.find_by_id(params[:id], :include => :re_artifact_properties) || ReAttachment.new
-    @artifact = @re_attachment.re_artifact_properties
+
+  def edit_hook_after_artifact_initialized params
+
+  end
+  
+  def edit_hook_validate_before_save(params, valid)
+    attachment_hash = params["attachment"] || {}
+    attachment_uploaded = @artifact.attach_file(attachment_hash)
     
-    if request.post?
-      @re_attachment.attributes = params[:re_attachment]
-      add_hidden_re_artifact_properties_attributes @re_attachment
-      
-      attachment_hash = params["attachment"] || {}
-      attachment_uploaded = @re_attachment.attach_file(attachment_hash)
-
-      return if !attachment_uploaded
-
-			flash[:notice] = t(:re_attachment_saved, {:name => @re_attachment.name}) if save_ok = @re_attachment.save
-
-      if save_ok && ! params[:parent_artifact_id].empty?
-        @parent = ReArtifactProperties.find(params[:parent_artifact_id])
-        @re_attachment.set_parent(@parent, 1)
-      end
-      
-      if save_ok
-        # Saving of user defined Fields (Building Blocks)
-        ReBuildingBlock.save_data(@re_attachment.re_artifact_properties.id, params[:re_bb])
-      end
-			
-      redirect_to :action => 'edit', :id => @re_attachment.id and return if save_ok
-    end
+    attachment_uploaded
   end
 
   def download_or_show
