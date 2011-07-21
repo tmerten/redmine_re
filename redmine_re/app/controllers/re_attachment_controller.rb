bdb3e16d1f7599350a81b727b2dded2bca886370
dmarko2s <dominic.markowski@smail.inf.h-brs.de>
2011-07-21 12:58:16 +0000

    Filesownload works (#202)

--- redmine_re/app/controllers/re_attachment_controller.rb
+++ redmine_re/app/controllers/re_attachment_controller.rb
@@ -15,12 +15,23 @@ class ReAttachmentController < RedmineReController
 
   def download_or_show
     @re_attachment = ReAttachment.find(params[:id])
+    @re_attachment.attachment = Attachment.find(params[:attachment_id])
     @re_attachment.attachment.increment_download
-
+    
     # images are sent inline
-    send_file @re_attachment.attachment.diskfile, :filename => filename_for_content_disposition(@re_attachment.attachment.filename),
-                                    :type => Redmine::MimeType.of(@re_attachment.attachment.filename),
-                                    :disposition => (@re_attachment.attachment.image? ? 'inline' : 'attachment')
+    send_file @re_attachment.attachment.diskfile, 
+      :filename => filename_for_content_disposition(@re_attachment.attachment.filename),
+      :type => detect_content_type(@re_attachment.attachment),
+      :disposition => (@re_attachment.attachment.image? ? 'inline' : 'attachment')
+      
+      
   end
 
+  def detect_content_type(attachment)
+    content_type = attachment.content_type
+    if content_type.blank?
+      content_type = Redmine::MimeType.of(attachment.filename)
+    end
+    content_type.to_s
+  end
 end
