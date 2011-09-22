58b200d185d84d573a46726d9b76108f0e1ac61e
tmerte2m <thorsten.merten@h-brs.de>
2011-09-22 15:02:20 +0000

    deleted generated tests which objects do not exist and thus prevent the test run from completing. er. deleted clutter.

--- redmine_re/test/unit/re_artifact_properties_test.rb
+++ redmine_re/test/unit/re_artifact_properties_test.rb
@@ -1,38 +1,4 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ReArtifactPropertiesTest < ActiveSupport::TestCase
-  fixtures :projects, :re_artifact_properties
-
-  def setup
-    @ecookbook = projects(:projects_001)
-    @changed_name = "Name Property Changed"
-    @changed_name2 = "Name Property Changed Again"
-    
-    User.current = nil
-  end
-  
-  def test_pseudo_project_artifact
-
-    project_properties = ReArtifactProperties.find_by_artifact_type "Project"
-    
-    @ecookbook.save
-    assert_nil project_properties, "Project artifact should not be created if the project did not change"
-    
-    @ecookbook.name = @changed_name
-    @ecookbook.save
-    project_properties = ReArtifactProperties.find_by_artifact_type "Project"
-    
-    # changed project name and saved (new project properties)
-    assert_not_nil project_properties, "Project artifact should be created if the project has changed"
-    assert_equal @changed_name, project_properties.name, "Project artifact should have the same name as the project"
-    
-    project_properties_id = project_properties.id
-    @ecookbook.name = @changed_name2
-    @ecookbook.save
-    project_properties = ReArtifactProperties.find_by_artifact_type "Project"
-
-    assert_not_nil project_properties, "Project artifact should be created if the project has changed"
-    assert_equal project_properties_id, project_properties.id, "The existing project artifact should be updated"
-    assert_equal @changed_name2, project_properties.name, "Project artifact should have the same name as the project"
-  end
 end
