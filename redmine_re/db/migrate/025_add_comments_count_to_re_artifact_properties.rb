62225104133d2d772765c5fd5ce4603f67deb717
dmarko2s <dominic.markowski@smail.inf.h-brs.de>
2011-10-05 13:11:54 +0000

    adding migration script for comments

---
+++ redmine_re/db/migrate/025_add_comments_count_to_re_artifact_properties.rb
@@ -0,0 +1,9 @@
+class AddReBuildingBlocksInheritingText < ActiveRecord::Migration
+  def self.up
+    add_column :re_artifact_properties, "comments_count", :integer
+  end
+
+  def self.down
+    remove_column :re_artifact_properties, "comments_count", :integer
+  end
+end
