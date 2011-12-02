7dbfaa4553bec32960b42edf26b028bc3d75c180
djuepp2s <daniela.jueppner@smail.inf.h-brs.de>
2011-12-02 12:40:29 +0000

    Added some documentation.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,17 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper
 # Ensure that we are using the temporary fixture path
 Engines::Testing.set_fixture_path
 
+# This method id needed to be able to call the 
+# well-known "logger.debug"-command inside tests.
 def logger
   RAILS_DEFAULT_LOGGER
 end
 
+
+# This method extracts all messages from the given 
+# error_hash and stores them in an array. This allows
+# to analyse the build up error messages more easily
+# during tests. 
 def extract_error_messages(hash)
   messages = []
   hash.keys.each do |bb|
@@ -20,6 +27,10 @@ def extract_error_messages(hash)
   messages
 end
 
+# As the proper saving of building blocks contains the calling
+# of the additional_work_before/after_saving_strategies, this 
+# saving is encapsulated in this method. It eases the many saving-
+# proceedings needed during tests.
 def save_building_block_completely(re_bb, params)
   re_bb.attributes = params[:re_building_block]
   re_bb = ReBuildingBlock.do_additional_work_before_save(re_bb, params)
