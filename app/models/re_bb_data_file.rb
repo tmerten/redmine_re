58250f0544c9ac501de33af7b5ac7362ad5185fc
dmarko2s <dominic.markowski@smail.inf.h-brs.de>
2012-02-13 14:32:39 +0000

    Adding basic files for file building block

---
+++ app/models/re_bb_data_file.rb
@@ -0,0 +1,7 @@
+class ReBbDataFile < ActiveRecord::Base
+  unloadable
+  
+  belongs_to :re_bb_text
+  belongs_to :re_artifact_properties
+  
+end
---
+++ app/models/re_bb_file.rb
@@ -0,0 +1,61 @@
+class ReBbFile < ReBuildingBlock
+  unloadable
+  
+ 
+  @@data_form_partial_strategy = 're_building_block/re_bb_file/data_form'
+  @@multiple_data_form_partial_strategy = 're_building_block/re_bb_file/multiple_data_form'
+  @@additional_work_before_save_strategies = {}
+  @@additional_work_after_save_strategies = {}
+  @@validation_strategies = {}
+  @@validation_whole_data_strategies = {}  
+  
+  def data_form_partial_strategy
+    @@data_form_partial_strategy
+  end
+  
+  def multiple_data_form_partial_strategy
+    @@multiple_data_form_partial_strategy
+  end
+  
+  def additional_work_before_save_strategies
+    @@additional_work_before_save_strategies
+  end
+    
+  def additional_work_after_save_strategies
+    @@additional_work_after_save_strategies
+  end
+  
+  def validation_strategies
+    @@validation_strategies
+  end
+  
+  def validation_whole_data_strategies
+    @@validation_whole_data_strategies
+  end
+
+  def save_datum(datum_hash, artifact_properties_id)    
+    datum_hash.keys.each do |id|
+      # Data should only be saved if no other data object with
+      # the same content is existent.
+      attributes = datum_hash[id]
+      
+      logger.debug(attributes.to_yaml)
+      if ReBbDataLink.find(:first, :conditions => {:url => attributes[:url], :re_artifact_properties_id => artifact_properties_id, :re_bb_link_id => self.id}).nil?
+        # With multiple values possible, the saving of empty data should be forbidden
+        unless (attributes[:url].nil? or attributes[:url] == "")
+          if attributes[:description].blank?
+            attributes[:description] = attributes[:url]
+          end
+          #Try to find a bb_data_object with the given id. 
+          #If no matching object is found, create a new one
+          bb_data = ReBbDataLink.find_by_id(id) || ReBbDataLink.new
+          bb_data.attributes = attributes
+          bb_data.re_artifact_properties_id = artifact_properties_id
+          bb_data.re_bb_link_id = self.id
+          bb_data.save
+        end      
+      end
+    end
+  end 
+  
+end
