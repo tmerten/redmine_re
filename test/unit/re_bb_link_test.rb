c22381965b4f9fa77a90f799f6faf7d30f48391d
tmerte2m <thorsten.merten@h-brs.de>
2012-02-24 15:38:57 +0000

    changed fixture. tests running now

--- test/unit/re_bb_link_test.rb
+++ test/unit/re_bb_link_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ReBbLinkTest < ActiveSupport::TestCase
-  fixtures :re_bb_links
+  fixtures :re_building_blocks
 
   # Replace this with your real tests.
   def test_truth
