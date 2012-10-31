d82b2551245d3aa7bb7042897a9f16c1d58f12cb
tmerte2m <thorsten.merten@h-brs.de>
2012-10-31 10:49:49 +0000

    added autosave from task to subtasks

--- app/models/re_task.rb
+++ app/models/re_task.rb
@@ -3,7 +3,7 @@ class ReTask < ActiveRecord::Base
 
   acts_as_re_artifact
 
-  has_many :re_subtasks, :inverse_of => :re_task, :dependent => :destroy, :order => :position
+  has_many :re_subtasks, :inverse_of => :re_task, :dependent => :destroy, :order => :position, :autosave => true
 
   accepts_nested_attributes_for :re_subtasks, :allow_destroy => true,
     :reject_if => proc { |attributes| attributes['name'].blank? && attributes['solution'].blank? }
