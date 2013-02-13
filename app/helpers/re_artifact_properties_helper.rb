d93266296b16ac63ae8ebfceaf690181864d061a
kkolle2s <robert.kolle@smail.inf.h-brs.de>
2013-02-13 17:25:45 +0000

    Feature #470. Implemented ReArtifactProperties as Attachment. Uses Redmine basis functionalities now, for better maintenance. Added views and actions for new and update. Edited styles a bit for attachments and some for fun.

--- app/helpers/re_artifact_properties_helper.rb
+++ app/helpers/re_artifact_properties_helper.rb
@@ -4,17 +4,31 @@ module ReArtifactPropertiesHelper
   def artifact_heading(artifact)
     h("#{rendered_artifact_type(artifact.artifact_type)} ##{artifact.id} ")
   end
-  
-  def format_expansions_field_name (field_html, use_case_step_id)
-     begin
-        field_html["re_artifact_properties[artifact_attributes][re_use_case_steps_attributes][re_use_case_step_expansions_attributes]"] = "re_artifact_properties[artifact_attributes][re_use_case_steps_attributes][][re_use_case_step_expansions_attributes]["+use_case_step_id.to_s+"]"
-        field_html["["+use_case_step_id.to_s+"][]"] = "["+use_case_step_id.to_s+"]"
-     rescue
-       logger.debug("The re Use Case Expansion name replace method (re_application_helper) somtimes fails !!!!!!!!!!!!")
-       logger.debug(field_html)
-     end
-     
-     field_html
+
+  def autocomplete_issue
+    query = '%' + params[:issue_subject].gsub('%', '\%').gsub('_', '\_').downcase + '%'
+    issues_for_ac = Issue.find(:all, :conditions => ['subject like ? AND project_id=?', query, @project.id])
+    list = '<ul>'
+    issues_for_ac.each do |issue|
+      list << '<li ' + 'id='+issue.id.to_s+'>'
+      list << issue.subject.to_s+' ('+issue.id.to_s+')'
+      list << '</li>'
+    end
+
+    list << '</ul>'
+    render :text => list
   end
-  
+
+  def format_expansions_field_name (field_html, use_case_step_id)
+    begin
+      field_html["re_artifact_properties[artifact_attributes][re_use_case_steps_attributes][re_use_case_step_expansions_attributes]"] = "re_artifact_properties[artifact_attributes][re_use_case_steps_attributes][][re_use_case_step_expansions_attributes]["+use_case_step_id.to_s+"]"
+      field_html["["+use_case_step_id.to_s+"][]"] = "["+use_case_step_id.to_s+"]"
+    rescue
+      logger.debug("The re Use Case Expansion name replace method (re_application_helper) somtimes fails !!!!!!!!!!!!")
+      logger.debug(field_html)
+    end
+
+    field_html
+  end
+
 end
