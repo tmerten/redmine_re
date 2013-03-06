f9c527f92c680ec0829014b82a188b58f51adc25
tmerte2m <thorsten.merten@h-brs.de>
2013-03-06 16:18:51 +0000

    some minor updates in ReUseCase Show view and the way the artifact specific fields are rendered

--- app/models/re_use_case_step_expansion.rb
+++ app/models/re_use_case_step_expansion.rb
@@ -4,4 +4,9 @@ class ReUseCaseStepExpansion < ActiveRecord::Base
   
   validates :re_use_case_step, :presence => true
   validates :re_expansion_type, :inclusion => { :in => 1..3 }
+  
+  def expansion_type_translation_key
+    translation_keys = { 1 => :re_exception_expansion, 2 => :re_data_expansion, 3 => :re_other_expansion }
+    translation_keys[re_expansion_type]
+  end
 end
