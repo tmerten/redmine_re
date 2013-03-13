8468c9806c00d172ecd3226c70bcbe47846b0122
eschro2s <eva.schrowangen@smail.inf.h-brs.de>
2013-03-13 13:23:17 +0000

    implements #521 Users can drop elements from on the traces to and traces from fields in the show view and add new relations this way

--- config/routes.rb
+++ config/routes.rb
@@ -70,5 +70,5 @@ RedmineApp::Application.routes.draw do
   #end
   
   match "projects/:project_id/diagram_preview/:diagram_id" => 'requirements#sendDiagramPreviewImage'
-  
+  match "/addRelation" => 'requirements#addRelation'
 end
