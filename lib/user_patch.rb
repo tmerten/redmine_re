391cdd29789c0f8a84d111f16fcb86ce681232b0
kkolle2s <robert.kolle@smail.inf.h-brs.de>
2013-04-01 21:06:56 +0000

    Remonde old rateful_ajax. UserPatch is still not used in init.rb

--- lib/user_patch.rb
+++ lib/user_patch.rb
@@ -3,8 +3,6 @@ require_dependency 'user'
 module UserPatch
 
   def self.included(base)
-    base.ajaxful_rater
-
     base.class_eval do
       unloadable
       has_many :created_re_queries, :class_name => 'ReQuery', :foreign_key => 'created_by'
