f16ba8f7df55586bdb35475304b8674b3ab49c3c
sklein2s <sebastian.klein@smail.inf.h-brs.de>
2013-05-16 14:50:44 +0000

    Fix Bug double @re_artifact_properties

--- db/migrate/042_create_relationship_visualizations.rb
+++ db/migrate/042_create_relationship_visualizations.rb
@@ -1,42 +1,42 @@
 class CreateRelationshipVisualizations < ActiveRecord::Migration
   def self.up
     create_table :re_relationship_visualizations do |t|
-      t.column :user_id, :integer
-      t.column :project_id, :integer
-      t.column :artefakt_id, :integer
-      t.column :visualization_typ, :string
+      t.integer :user_id
+      t.integer :project_id
+      t.integer :artefakt_id
+      t.string :visualization_typ
 
-      t.column :re_attachment, :integer, :default => 1
-      t.column :re_goal, :integer, :default => 1
-      t.column :re_processword, :integer, :default => 1
-      t.column :re_rationale, :integer, :default => 1
-      t.column :re_requirement, :integer, :default => 1
-      t.column :re_scenario, :integer, :default => 1
-      t.column :re_section, :integer, :default => 1
-      t.column :re_task, :integer, :default => 1
-      t.column :re_user_profile, :integer, :default => 1
-      t.column :re_use_case, :integer, :default => 1
-      t.column :re_vision, :integer, :default => 1
-      t.column :re_workarea, :integer, :default => 1
+      t.integer :re_attachment, :default => 1
+      t.integer :re_goal, :default => 1
+      t.integer :re_processword, :default => 1
+      t.integer :re_rationale, :default => 1
+      t.integer :re_requirement, :default => 1
+      t.integer :re_scenario, :default => 1
+      t.integer :re_section, :default => 1
+      t.integer :re_task, :default => 1
+      t.integer :re_user_profile, :default => 1
+      t.integer :re_use_case, :default => 1
+      t.integer :re_vision, :default => 1
+      t.integer :re_workarea, :default => 1
       
-      t.column :dependency, :integer, :default => 1
-      t.column :conflict, :integer, :default => 1
-      t.column :rationale, :integer, :default => 1
-      t.column :refinement, :integer, :default => 1
-      t.column :part_of, :interger, :default => 1
-      t.column :parentchild, :integer, :default => 1
-      t.column :primary_actor, :integer, :default => 1
-      t.column :actors, :integer, :default => 1
-      t.column :diagram, :integer, :default => 1
+      t.integer :dependency, :default => 1
+      t.integer :conflict, :default => 1
+      t.integer :rationale, :default => 1
+      t.integer :refinement, :default => 1
+      t.integer :part_of, :default => 1
+      t.integer :parentchild, :default => 1
+      t.integer :primary_actor, :default => 1
+      t.integer :actors, :default => 1
+      t.integer :diagram, :default => 1
       
-      t.column :issue, :integer, :default => 1
+      t.integer :issue, :default => 1
       
-      t.column :max_deep, :integer, :default => 0
+      t.integer :max_deep, :default => 0
       
-      t.column :created_at, :datetime, :null => false
-      t.column :updated_at, :datetime, :null => false
-      t.column :created_by, :integer, :default => 0
-      t.column :updated_by, :integer, :default => 0
+      t.datetime :created_at, :null => false
+      t.datetime :updated_at, :null => false
+      t.integer :created_by, :default => 0
+      t.integer :updated_by, :default => 0
       end
   end
 
