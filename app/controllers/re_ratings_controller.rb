ac3e724d1305b31bc0764d4255d5ef1a78ea2e6f
Markowski <dmarkowski@steep.loc>
2018-08-06 15:25:48 +0200

    Fixing bug in ratings. Still not work because of ther bugs.

--- app/controllers/re_ratings_controller.rb
+++ app/controllers/re_ratings_controller.rb
@@ -3,10 +3,11 @@ class ReRatingsController < ApplicationController
 
   def create
     @rap = ReArtifactProperties.find_by_id(params[:re_artifact_properties_id])
-    @rating = ReRating.find_or_create_by_re_artifact_properties_id(params[:re_artifact_properties_id])
+    @rating = ReRating.find_or_create_by(re_artifact_properties_id: params[:re_artifact_properties_id])
     @rating.value = params[:re_rating][:value]
     @rating.user_id = User.current.id
-
+    @rating.save
+	
     respond_to do |format|
       if @rating.save
         format.html { redirect_to @rap, :notice => 'Your rating has been saved' }
